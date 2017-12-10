dfb5ae91542c67e50aa3a481184bf6076a00cd90
Guanchen Zhang <gzhang22@gmail.com>
2017-12-10 12:57:38 -0800

    add cmd args in data reading

--- src/dat_prep.R
+++ src/dat_prep.R
@@ -1,36 +1,28 @@
-# Prep data: read, process, clean
+#! /usr/bin/env Rscript 
+# dat_prep.R
+# 
+# This script reads the downloaded data, compresses the data, 
+# and saves it to a compressed format.
 
 library(tidyverse)
 
+args <- commandArgs(trailingOnly = TRUE)
+path_input <- args[1]
+path_output <- args[2]
 
-dat_raw_train <- read_csv("./data/exoTrain.csv")
+exit_status <- tryCatch({
+  dat_raw_train <- read_csv(path_input)
+  saveRDS(dat_raw_train, file = path_output)  # save the compressed
+  es <- 0
+}, warning = function (war) {
+  paste("Warning: ", war)
+  es <- 1
+}, error = function (err) {
+  paste("Error: ", err)
+  es <- 1
+}, finally = {
+  print("CSV loaded successfully.")
+})
 
-plot_flux_series <- function(star_idx, dat) {
-  h_train <- t(dat[star_idx,])
-  h_train <- h_train[2:length(h_train)]
-  h_train <- tibble(
-    time = 1:length(h_train),
-    flux = h_train
-  )
-  
-  h_train %>%
-    ggplot(aes(x = time, y = flux)) + geom_path()
-}
+exit_status
 
-plot_flux_hist <- function(start_idx, dat) {
-  h_train <- t(dat[star_idx,])
-  h_train <- h_train[2:length(h_train)]
-  h_train <- tibble(
-    time = 1:length(h_train),
-    flux = h_train
-  )
-  
-  h_train %>%
-    ggplot() + geom_histogram(aes(flux), bins = 40)
-}
-
-star_idx <- 3 #1525
-
-plot_flux_series(star_idx, dat_raw_train)
-
-plot_flux_hist(star_idx, dat_raw_train)
